64511c46d354ddcc9c4d66361c954085a170812c
杨园亮 <yangyuanliang@yangyuanliangdeMacBook-Pro.local>
2019-09-30 13:38:24 +0800

    记录网站运行时间

--- src/utils/config.js
+++ src/utils/config.js
@@ -2,7 +2,7 @@
 // 请把生产环境的 redirect_url，client_id 和 client_secret 中的 "****", 换成自己创建的 OAuth App 的具体参数即可。
 const config = {
   'oauth_uri': 'https://github.com/login/oauth/authorize',
-  'redirect_uri': 'http://localhost:3001/user/getUser',
+  'redirect_uri': 'http://118.31.9.84:3001/user/getUser',
   'client_id': '4b8e4dd18ee36f991386',
   'client_secret': '117548602c30104262bc7c8ffb9e37f319a031a5',
 };
